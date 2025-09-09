60072eb97f178795cff71e3d072aaf8bb5b06260
bhatMushtaq <mushtaqa502035@gmail.com>
2025-09-09 13:28:28 +0530

    AuthSettings....

---
+++ Intern/Intern/Common/AuthSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Intern.Common
+{
+    public class AuthSettings
+    {
+        public int EmailVerificationExpiryMinutes { get; set; }
+        public int ResetPasswordExpiryMinutes { get; set; }
+    }
+}
